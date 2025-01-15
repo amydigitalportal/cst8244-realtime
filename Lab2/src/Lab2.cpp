8ba0e3ba862607d8335cc0dc6660238b73b63cf6
Amy Novun <amy.digitalportal@gmail.com>
2025-01-15 00:01:04 -0500

    implement Hello World with gitignore

--- Lab2/src/Lab2.cpp
+++ Lab2/src/Lab2.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+
+void Println(string message) {
+	cout << message << endl;
+}
+
 int main() {
-	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	Println("Hello World from QNX Neutrino RTOS!!!");
+	Println("@author Amy Novun (novu0001@algonquinlive.com)");
+	Println("Unique trait: I hoard dark chocolate in my fridge as an emergency food supply. :)");
 	return 0;
 }
