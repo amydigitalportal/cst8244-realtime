17e5d93d456ab162ee7b90f004d6d7b12cb5ff09
Amy Novun <novu0001@algonquinlive.com>
2025-03-28 16:18:32 -0400

    pasted in res mgr solution from lab7

---
+++ a2_metronome/src/metronome.h
@@ -0,0 +1,14 @@
+/*
+ * metronome.h
+ *
+ *  Created on: Mar. 28, 2025
+ *      Author: Amy-PC
+ */
+
+#ifndef METRONOME_H_
+#define METRONOME_H_
+
+#define DEVICE_NAME "metronome"
+#define DEVICE_PATH "/dev/local/metronome"
+
+#endif /* METRONOME_H_ */
